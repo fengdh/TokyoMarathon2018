a3f7930b588dbdb5c9f1f425c8269c9ac6b31097
Feng Dihai <gfen@bbs.co.jp>
2018-03-14 13:50:26 +0900

    hack

--- docs/hackphoto.js
+++ docs/hackphoto.js
@@ -11,3 +11,13 @@ function toUrls(item) {
 	  thumb = TMPL.head + [p.event_id, p.photographer_id, p.photo_num, p.page_id, p.photo_id, p.photo_hash, TMPL.tail].join('_');
   return [full, thumb];
 }
+
+
+var cnt = 0, s = '';
+function padding(n) { return ('000' + cnt).slice(-3) }
+$.getJSON('common.json').then(r => {
+  r = r.map(toUrls);
+  //r = r.slice(1,13);
+  r = r.map( e => (cnt++, e.map((d, i) => 'http:' + d + ' ' + padding(cnt) + (i === 0 ? '-W' : '-T') +  '.jpg')));
+  return [].concat(...r)
+}).then(r => console.log(r.join('\r\n')));
