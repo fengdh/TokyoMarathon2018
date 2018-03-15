f49ef2ba48aedf855b3c1997b510ed95f4c44786
Feng Dihai <gfen@bbs.co.jp>
2018-03-15 15:41:55 +0900

    up

--- docs/album/download.ps1
+++ docs/album/download.ps1
@@ -1,11 +1,11 @@
-$folder = ".\"
+$folder = "..\img-f"
 $userAgent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Mobile Safari/537.36"
 $web = New-Object System.Net.WebClient
 $web.Headers.Add("user-agent", $userAgent)
-$web.Headers['cookie'] = "ALLSPORTS_SESS=;"
+$web.Headers['cookie'] = "ALLSPORTS_SESS=3ada7d3768ff6205a31bd975db437774527c73fc3a4586f48b62dd85b2fe5c7f;"
 $web
 
-Get-Content "D:\github\TokyoMarathon2018\docs\album\list.txt" |
+Get-Content "D:\github\TokyoMarathon2018\docs\album\list-f.txt" |
     Foreach-Object {
 				$item = $_.Split(" ",[System.StringSplitOptions]::RemoveEmptyEntries)
 				""
--- docs/album/hackphoto.js
+++ docs/album/hackphoto.js
@@ -27,3 +27,34 @@ function outputList(cnt, url) {
 
 outputList(0, 'album/allsp.json')
 	.then( _ => outputList(300, 'album/common.json'));
+
+
+	//image.photocreate.jp/r/tn_624719_1763_2599542_915717475_8311b66446f7ed29b01315821f3c1420_1521044339/pc_watermark_99_h/0/1186x853/47x0_1186x853/ 002-W.jpg
+	"//image.photocreate.jp/r/tn_624719_1763_2599542_915717475_8311b66446f7ed29b01315821f3c1420_1521044589/pc_watermark_99_h/0/1186x853/47x0_1186x853/ 002-W.jpg
+
+//allsports.jp/photo/photo_624719_1763_2599542_505586_915717475_8311b66446f7ed29b01315821f3c1420_t  002-T.jpg"
+
+
+// simple solution
+var now = Math.floor(new Date().getTime() / 1000);
+var cnt = 0;
+
+function toUrls(item) {
+  return item.detail.replace(/\/pc_watermark_3_([vh])\//, '/pc_watermark_99_$1/')
+                    .replace(/(\/[0123]\/).*$/, '$1')
+                    .replace(/(.*)_(.*)(\/pc_watermark_)/, '$1_' + now + '$3');
+}
+
+function padding(n) { return ('000' + cnt).slice(-3) }
+
+function outputList(list) {
+	return $.when(list).then(r => {
+	  r = r.map(toUrls);
+// 	  r = r.slice(1,13);
+	  r = r.map( d => 'http:' + d + ' W' + padding(++cnt) + '.jpg');
+	  console.log(r.join('\r\n'));
+	  return r;
+	});
+}
+
+outputList(pc8photos.models);
